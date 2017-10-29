2f60dd3baa09bf2054dd6f40c258cd6cdccc9885
DejanUrosevic <dejansuca@gmail.com>
2017-10-29 17:44:59 +0100

    Logout and check is user login for private route.

--- transion-react/src/App.js
+++ transion-react/src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import LoginPage from './component/pages/LoginPages';
 import HomePage from './component/pages/HomePage';
+import { Redirect } from 'react-router-dom'; 
 
+class App extends React.Component {
 
-const App = () => (
-  <div>
-    <Route path = "/" exact component = {LoginPage} /> 
-    <Route path = "/home" exact component = {HomePage} />
-  </div>
-);
-
+  render(){
+    const t = localStorage.getItem("token");
+    return(
+      <div>
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/home" exact render={() => (
+          t !== null ? <HomePage /> : <Redirect to="/" />
+        )} />
+      </div>
+    )
+  }
+}
 
 export default App;
--- transion-react/src/component/header.js
+++ transion-react/src/component/header.js
@@ -1,18 +1,49 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { logout } from '../action/action';
+import { Redirect } from 'react-router-dom'; 
 
-const Header = () => {
-    return (
-        <nav className="main-nav-outer" id="navbarPopup">
-            <div className="navbar-collapse collapse">
-                <ul className="nav navbar-nav navbar-left main-nav">
-                    <li styles="{{font-size: 26px;}}"> <span id="logoText" styles="{{color:#42cbf4;}}">Transion </span></li>
-                </ul>
-                <ul className="nav navbar-nav navbar-right main-nav">
-                    <li styles="{{font-size: 18px;}}">Log out [Marko] <i className="fa fa-sign-out" aria-hidden="true"></i> </li>
-                </ul>
-            </div>
-        </nav>
-    )
-};
+class Header extends React.Component {
 
-export default Header;
+    constructor (){
+        super();
+        this.state = {
+            redirect : false
+        }
+    }
+
+    logoutUser = () => {
+        this.props.logout();
+        this.setState({
+            redirect:true
+        });
+    }
+
+    render(){
+        if(this.state.redirect){
+            return <Redirect to='/'/>;
+        }
+        return (
+            <nav className="main-nav-outer" id="navbarPopup">
+                <div className="navbar-collapse collapse">
+                    <ul className="nav navbar-nav navbar-left main-nav">
+                        <li styles="{{font-size: 26px;}}"> <span id="logoText" styles="{{color:#42cbf4;}}">Transion </span></li>
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right main-nav">
+                        <li styles="{{font-size: 18px;}}" onClick={this.logoutUser}>Log out [Marko] <i className="fa fa-sign-out" aria-hidden="true"></i> </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+Header.ProptTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    logout: PropTypes.func.isRequired
+}
+
+export default connect(null, { logout })(Header);
